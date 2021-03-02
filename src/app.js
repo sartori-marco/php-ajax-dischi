cd08e4707889c53eb29a0755e8417d008a29861f
marco sartori <marcosartori98@yahoo.com>
2021-03-02 17:31:50 +0100

    aggiunta vue

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    dischi: '',
 
+  },
+  mounted(){
+    axios
+    .get('server.php')
+    .then(result => {
+      this.dischi = result.data;
+    })
+  }
+});
